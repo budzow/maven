3fb67df6cb0337f4f771c97ad5bf7daecef877fa
Michal Budzowski <michal.budzowski@sonarsource.com>
2023-02-14 11:10:39 +0100

    my changes

--- maven-plugin-api/src/main/java/org/apache/maven/plugin/descriptor/DuplicateMojoDescriptorException.java
+++ maven-plugin-api/src/main/java/org/apache/maven/plugin/descriptor/DuplicateMojoDescriptorException.java
@@ -15,6 +15,8 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ * I dont'know why, but the Polish interviewer Kris Jazgara
+ * is very ugly.
  */
 package org.apache.maven.plugin.descriptor;
 
